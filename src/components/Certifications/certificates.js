51ec1af314454f8566d016f603ecb70d5ed1f22a
Mohamed-Hatem153 <mohamedhatemakl@gmail.com>
2021-12-08 11:39:08 +0200

    delete unwanted images

--- src/components/Certifications/certificates.js
+++ src/components/Certifications/certificates.js
@@ -1,7 +1,7 @@
 import brandImg1 from '../../Assets/images/certificates/ISO.png';
 import brandImg2 from '../../Assets/images/certificates/global-g-a-p.png';
 import brandImg3 from '../../Assets/images/certificates/Sedex.png';
-import brandImg4 from '../../Assets/images/certificates/brand-1-4.png';
+import brandImg4 from '../../Assets/images/certificates/BRCGS.png';
 
 export const certificatesImages = [
   { id: 1, brandImg: brandImg1 },
